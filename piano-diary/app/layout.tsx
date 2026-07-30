4b30c1e4c446ec5a8422566a73f9bf42cf6f2604
xxuwin <zxcvb20475@gmail.com>
2026-07-31 00:37:21 +0900

    chande font style

--- piano-diary/app/layout.tsx
+++ piano-diary/app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const noto = Noto_Sans_KR({
+  variable: "--font-noto",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: [
+    "400",
+    "500",
+    "700",
+    "800"
+  ],
 });
 
 export const metadata: Metadata = {
@@ -24,9 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+  lang="ko"
+  className={`${noto.variable} h-full antialiased`}
+>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
